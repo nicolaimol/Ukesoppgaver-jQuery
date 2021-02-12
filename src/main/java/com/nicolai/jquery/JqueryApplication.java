27d2587cc91a30b6c146dc799495deb7e6fab24d
s341833 <56063595+s341833@users.noreply.github.com>
2021-02-12 11:31:45 +0100

    Initial commit

---
+++ src/main/java/com/nicolai/jquery/JqueryApplication.java
@@ -0,0 +1,13 @@
+package com.nicolai.jquery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JqueryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JqueryApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/nicolai/jquery/JqueryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nicolai.jquery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JqueryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
